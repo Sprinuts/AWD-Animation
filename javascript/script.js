24910f0fabfa826f8daaaa885e92fb5b9a3492e5
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2024-02-03 14:11:59 +0800

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,37 @@
 let cursor = document.querySelector('#cursor');
         let body = document.querySelector('body'); 
+        let isHoldingClick = false;
+
+        document.onmousedown = function(e) {
+            isHoldingClick = true;
+            createAndAppendElement(e);
+        }
+
+        document.onmouseup = function() {
+            isHoldingClick = false;
+        }
+
         document.onmousemove = function(e){
+            if (isHoldingClick) {
+                createAndAppendElement(e);
+            }
             body.style.backgroundPositionX = e.pageX/-4 + 'px';
             body.style.backgroundPositionY = e.pageY/-4 + 'px';
             cursor.style.top = e.pageY + 'px';
             cursor.style.left = e.pageX + 'px';
+        }
 
+        function createAndAppendElement(e) {
             let element = document.createElement('div');
             element.className = 'element';
-            body.prepend(element);
-
-            //random mover
-            element.style.left = cursor.getBoundingClientRect().x + 'px';
-            element.style.top = cursor.getBoundingClientRect().y - 10 + 'px';
+            element.style.left = e.pageX + 'px';
+            element.style.top = e.pageY + 'px';
+            element.innerHTML = randomText();
+            body.appendChild(element);
 
             setTimeout(function(){
-                let text = document.querySelectorAll('.element')[0];
-                directionX = Math.random() < .5 ? -1 : 1,
-                directionY = Math.random() < .5 ? -1 : 1
-
-                text.style.left = parseInt(text.style.left) - (directionX * (Math.random() * 200)) + 'px';
-                text.style.top = parseInt(text.style.top) - (directionY * (Math.random() * 200)) + 'px';
-                text.style.opacity = 0;
-                text.style.transform = 'scale(0.25)';
-                text.innerHTML = randomText();
-
-                setTimeout(function(){
-                    element.remove()
-                },1000)
-
-            },10)
+                element.remove();
+            }, 1000);
         }
 
         function randomText(){
